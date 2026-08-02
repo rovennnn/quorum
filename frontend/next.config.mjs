da431c2734cbcb06b946e91a1724a8881b994ea2
Rawrven <168084271+rovennnn@users.noreply.github.com>
2026-08-02 23:50:41 +0800

    Add webpack fallback configuration to next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      encoding: false,
+      "pino-pretty": false,
+    };
+    return config;
+  },
 };
 export default nextConfig;
