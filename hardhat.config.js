a29ab28be36fdf3b4d1b53253dd8c0969c529267
Rawrven <168084271+rovennnn@users.noreply.github.com>
2026-08-02 18:10:49 +0800

    Add Hardhat plugins for ethers and testing

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,6 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-ethers");
+require("@nomicfoundation/hardhat-chai-matchers");
+require("@nomicfoundation/hardhat-network-helpers");
 require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
